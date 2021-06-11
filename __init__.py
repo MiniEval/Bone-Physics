80e8f8339a7f6ec57a1ebae1c887a30a17e8d006
MiniEval <85723511+MiniEval@users.noreply.github.com>
2021-06-11 14:33:17 +1000

    Initial Release

---
+++ __init__.py
@@ -0,0 +1,43 @@
+bl_info = {
+    "name": "Cloth-Based Bone Physics",
+    "description": "Workflow automation of cloth collision setups for armature bones.",
+    "blender": (2, 92, 0),
+    "category": "Physics",
+    "support": "COMMUNITY",
+    "version": (1, 0)
+}
+
+
+modulesNames = ['base_manager', 'operators', 'mesh_helpers']
+
+import sys
+import importlib
+
+modulesFullNames = {}
+for currentModuleName in modulesNames:
+    modulesFullNames[currentModuleName] = ('{}.{}'.format(__name__, currentModuleName))
+
+for currentModuleFullName in modulesFullNames.values():
+    if currentModuleFullName in sys.modules:
+        importlib.reload(sys.modules[currentModuleFullName])
+    else:
+        globals()[currentModuleFullName] = importlib.import_module(currentModuleFullName)
+        setattr(globals()[currentModuleFullName], 'modulesNames', modulesFullNames)
+
+
+def register():
+    for currentModuleName in modulesFullNames.values():
+        if currentModuleName in sys.modules:
+            if hasattr(sys.modules[currentModuleName], 'register'):
+                sys.modules[currentModuleName].register()
+
+
+def unregister():
+    for currentModuleName in modulesFullNames.values():
+        if currentModuleName in sys.modules:
+            if hasattr(sys.modules[currentModuleName], 'unregister'):
+                sys.modules[currentModuleName].unregister()
+
+
+if __name__ == "__main__":
+    register()
